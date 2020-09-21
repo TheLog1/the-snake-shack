e95213f53c5317b45b8dad0febd6397dc2c00e09
TheLog <lgscustomww@gmail.com>
2020-09-21 14:19:37 -0500

    Changed production api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://peaceful-wave-30757.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
